de12dd7ce0f4ba5ff572d25c11649dd1f6dcc0c1
Joanne <31201722jh@gmail.com>
2023-01-14 18:21:47 -0500

    Add level selector
    - Added the basic structure for a level selector (W.I.P.)
    - Basic graphic changes (colour)
    - Change platform sizes

--- project2.py
+++ project2.py
@@ -2,7 +2,7 @@ from pygame import *
 
 font.init()
 myClock = time.Clock()
-comicFont = font.SysFont("Comic Sans MS", 25)
+timesFont = font.SysFont("Times New Roman", 25)
 
 width, height = 1280, 720
 screen = display.set_mode((width, height))
@@ -15,6 +15,10 @@ GREEN = (0, 255, 0)
 YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 
+DARKTUYU = (23, 23, 41)
+MIDTUYU = (80, 81, 124)
+LIGHTTUYU = (206, 74, 125)
+
 running = True
 status = "menu"
 
@@ -25,14 +29,19 @@ textPos = [(i[0]+20, i[1]+20) for i in menuRects]
 stage = 0
 
 def menu():
-    screen.fill((147, 112, 219))
+    screen.fill(MIDTUYU)
     screen.blit(transform.smoothscale(image.load("images/WHATR.jpg"), (800, 720)), (0, 0))
     [draw.rect(screen, WHITE, i) for i in menuRects]
     [draw.rect(screen, BLACK, i, 5) for i in menuRects]
-    [screen.blit(comicFont.render(i, True, BLACK),(j)) for i,j in zip(menuText, textPos)]
+    [screen.blit(timesFont.render(i, True, BLACK),(j)) for i,j in zip(menuText, textPos)]
 
     display.flip()
 
+def levels():
+    global status # TEMPORARY
+    screen.fill((80, 81, 124))
+    display.flip()
+    status = "play" # NEEDS TO BE UPDATED TO A BUTTON
 # instructions 
 
 def instructions():
@@ -44,9 +53,9 @@ stage = 1
  # left top width height
 playerRect = Rect(55, 325, 10, 50) # Player
 
-platforms = [0, [Rect(100, 400, 200, 50), Rect(750, 325, 200, 50), Rect(600, 200, 200, 50)], [Rect(100, 200, 400, 20)]]
-platformsMain = [0, [Rect(0, 500, 1280, 100)], [Rect(0, 600, 1280, 100)]] # Platforms that don't allow you to "jump down".
-walls = [0, [Rect(-1, 0, 1, height), Rect(width/2, 400, 50, 300), Rect(1230, 150, 50, 320)], []]
+platforms = [0, [Rect(100, 400, 200, 25), Rect(750, 325, 200, 25), Rect(700, 200, 100, 25)], [Rect(100, 200, 400, 20)]]
+platformsMain = [0, [Rect(0, 500, 640, 100), Rect(0, width-playerRect.width, 0, 1)], [Rect(0, 600, 1280, 100)]] # Platforms that don't allow you to "jump down".
+walls = [0, [Rect(-1, 0, 1, height), Rect(width/2, 400, 50, 300), Rect(1230, 150, 50, 315)], []]
 
 X = 0
 Y = 1
@@ -73,21 +82,21 @@ def stages(playerRect):
     if playerRect[0] < 0 and stage != 1:
         playerRect[X] = width - playerRect[W]
         stage -= 1
-        playerRect[Y] = platformsMain[stage][0].y - playerRect.height
+        playerRect[Y] = platformsMain[stage][-1].y - playerRect.height
         print(playerRect)
         print(platformsMain[stage][0].y)
 
 def drawScene():
-    screen.fill(WHITE)
+    screen.fill(MIDTUYU)
     drawPlayer()
 
-    [draw.rect(screen, BLUE, i) for i in platforms[stage]]
-    [draw.rect(screen, BLACK, i) for i in platformsMain[stage]]
-    [draw.rect(screen, BLACK, i) for i in walls[stage]]
+    [draw.rect(screen, LIGHTTUYU, i) for i in platforms[stage]]
+    [draw.rect(screen, DARKTUYU, i) for i in platformsMain[stage]]
+    [draw.rect(screen, DARKTUYU, i) for i in walls[stage]]
     display.flip()
 
 def drawPlayer():
-    draw.rect(screen, RED, playerRect)  
+    draw.rect(screen, (231, 220, 216), playerRect)  
 
 def movePlayer(playerRect):
     v[X] = 0
@@ -156,8 +165,10 @@ while running:
 
     if status == "menu":
         menu()
-    elif status == "levels":
+    elif status == "instructions":
         instructions()
+    elif status == "levels":
+        levels()
     elif status == "play":
         stages(playerRect)
         movePlayer(playerRect)
@@ -167,9 +178,9 @@ while running:
 
     if mb[0] and status == "menu":
         if menuRects[0].collidepoint(mx, my):
-            status = "play"
-        if menuRects[1].collidepoint(mx, my):
             status = "levels"
+        if menuRects[1].collidepoint(mx, my):
+            status = "instructions"
         if menuRects[3].collidepoint(mx, my):
             running = False
             
